de73d5452eb67375afe2e84ed7053a3c53dc5c8f
Dobin <dobin.rutishauser@gmail.com>
2024-04-03 19:22:48 +0100

    feature: show iat in web ui

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ import os
 import logging
 from typing import List, Tuple
 
+from pe.superpe import SuperPe
 from model.defs import *
 
 views = Blueprint('views', __name__)
@@ -13,3 +14,19 @@ logger = logging.getLogger("Views")
 @views.route("/")
 def index():
     return render_template('index.html')
+
+
+@views.route("/exes/<exe_name>")
+def exe_view(exe_name):
+    path = "{}/{}".format(PATH_EXES, exe_name)
+    superpe = SuperPe(path)
+    return render_template('exe.html', superpe=superpe, iat=superpe.get_iat_entries())
+
+
+@views.route("/exes")
+def exes_view():
+    exes = []
+    for file in os.listdir(PATH_EXES):
+        exes.append(file)
+    return render_template('exes.html', exes=exes)
+
