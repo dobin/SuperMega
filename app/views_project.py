15444ae1254ddc4f3c38d5036b0afe444c38c107
CapitaineHadd0ck <58261410+CapitaineHadd0ck@users.noreply.github.com>
2025-03-21 17:45:40 +0100

    Fix .rdata max gap size display

--- app/views_project.py
+++ app/views_project.py
@@ -89,7 +89,7 @@ def project(name):
         
         has_rodata_section = superpe.has_rodata_section()
         if has_rodata_section:
-            superpe.get_rdata_rangemanager().find_largest_gap()
+            data_sect_largest_gap_size = superpe.get_rdata_rangemanager().find_largest_gap()
         unresolved_dlls = pe.dllresolver.unresolved_dlls(superpe)
 
     project_dir = os.path.dirname(os.getcwd() + "\\" + project.settings.main_dir)
